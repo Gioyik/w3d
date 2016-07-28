07241ebb5921a181ce3695e220fdc9b2b144deb2
Giovanny Andres Gongora Granada <gioyik@gmail.com>
2016-07-28 03:02:42 -0500

    Removing Johnny Five excecution

--- server/server.js
+++ server/server.js
@@ -3,11 +3,11 @@ var server = require('http').Server(app);
 var io = require('socket.io')(server);
 var five = require("johnny-five");
 var Raspi = require("raspi-io");
-var board = new five.Board({
+/*var board = new five.Board({
   io: new Raspi(),
   repl: false,
   debug: false,
-});
+});*/
 
 server.listen(80);
 
@@ -18,7 +18,7 @@ io.on('connection', function (socket) {
 io.on('ledBlink', function(data) {
   console.log('ledBlink');
   board.on("ready", function() {
-    var led = new five.Led(data);
-    led.blink();
+    /*var led = new five.Led(data);
+    led.blink();*/
   })
 });
