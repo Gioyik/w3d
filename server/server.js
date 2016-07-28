ad81be6d56f41e87eb465da54e6ccef7523a6873
Giovanny Andres Gongora Granada <gioyik@gmail.com>
2016-07-28 03:16:40 -0500

    Correct socket io structure for connection

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ var app = require('express')();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
 var five = require("johnny-five");
-var Raspi = require("raspi-io");
+//var Raspi = require("raspi-io");
 /*var board = new five.Board({
   io: new Raspi(),
   repl: false,
@@ -13,12 +13,11 @@ server.listen(80);
 
 io.on('connection', function (socket) {
   console.log("CONNECTION");
-});
-
-io.on('ledBlink', function(data) {
-  console.log('ledBlink');
-  board.on("ready", function() {
-    /*var led = new five.Led(data);
-    led.blink();*/
-  })
+  io.on('ledBlink', function(data) {
+    console.log('ledBlink');
+    /*board.on("ready", function() {
+      var led = new five.Led(data);
+      led.blink();
+    })*/
+  });
 });
