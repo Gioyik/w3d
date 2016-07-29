607a16bd6624fb2287c12541f3803f3b9e573a0e
Giovanny Andres Gongora Granada <gioyik@gmail.com>
2016-07-29 12:12:37 -0500

    Change on Lib structure and socket communication

--- client/client.js
+++ client/client.js
@@ -1,33 +1,11 @@
-var W3D;
-var socket;
+var socket = io.connect('http://191.110.134.15:8000');
 
-(function() {
-  'use strict';
-
-  var init = function() {
-    socket = io.connect('http://localhost:3000');
-  };
-
-  init.prototype = {
-
-    led: function(number) {
-      socket.on('connection', function () {
-        socket.emit('led:selection', number);
-      }
-    )},
-
-    blink: function(time) {
-      socket.on('connection', function () {
-        socket.emit('blink:time', time);
-      })
-    }
-  }
-
-  W3D = function() {
-    return new init();
-  }
-
-  if (window.$ == null) {
-    window.$ = w3d;
-  }
-}());
+var Web2Board = {
+  ledBlink: function(number) {
+    console.log('ledBlink: '+number);
+    socket.on('ready', function () {
+      socket.emit('ledBlink', number);
+    })
+    return this;
+  },
+};
