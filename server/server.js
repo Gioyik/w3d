f0896f3b4d38dfd21c1c1637ea3c09f476580992
Giovanny Andres Gongora Granada <gioyik@gmail.com>
2016-07-29 12:11:48 -0500

    Cross origin fixes

--- server/server.js
+++ server/server.js
@@ -1,7 +1,8 @@
-var app = require('express')();
+var express = require('express');
+var app = express();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
-var five = require("johnny-five");
+//var five = require("johnny-five");
 //var Raspi = require("raspi-io");
 /*var board = new five.Board({
   io: new Raspi(),
@@ -9,11 +10,25 @@ var five = require("johnny-five");
   debug: false,
 });*/
 
-server.listen(80);
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    res.header("Access-Control-Allow-Headers", "Content-Type");
+    res.header("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE, OPTIONS");
+    next();
+  }
+);
+
+app.use(express.static('public'));
+
+server.listen(8000);
+
+io.set('origins', '*:*');
 
 io.on('connection', function (socket) {
+  socket.emit('ready');
   console.log("CONNECTION");
-  io.on('ledBlink', function(data) {
+  socket.on('ledBlink', function(data) {
     console.log('ledBlink');
     /*board.on("ready", function() {
       var led = new five.Led(data);
